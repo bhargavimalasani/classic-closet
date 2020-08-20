ca14e2107fa0fd9a9342953173e048750c8c0c07
Lohith Reddy Nallappagari <lohith@macbook-pro.home>
2020-08-20 12:15:09 -0400

    displaying individual collection items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'; 
+import ShopPage from './pages/shop/shop.component.jsx'; 
 import './App.css';
+import {Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-     <HomePage />
+     <Route exact path='/' component={HomePage} />
+     <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import PreviewCollection from '../../components/preview-collection/preview-collection.component';
+
+import './shop.styles.scss';
+
+class ShopPage extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    render(){
+        const {collections} = this.state;
+        return (
+            <div className='shop-page'>
+                {collections.map(({id,...otherCollectionProps}) =>(
+                        <PreviewCollection key={id} {...otherCollectionProps}/>
+                ))}
+            </div>
+            );
+    }
+}
+
+export default ShopPage;
