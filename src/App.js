4f5f72cc4f8e1cad6e5ead54fd837340e77a4ce8
Lohith Reddy Nallappagari <lohith@macbook-pro.home>
2020-08-21 16:28:17 -0400

    Implemented firebase utils to store in database

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and sign-up/sign-in-and sign-up.component';
 import './App.css';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument } from './firebase/firebase.utils';
 import {Switch, Route} from 'react-router-dom';
 import Header from './components/header/header.component';
 
@@ -20,9 +20,22 @@ class App extends React.Component {
  
  
   componentDidMount(){
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-         this.setState({currentUser: user})
-         console.log(user);
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        
+         if(userAuth){
+            const userRef = await createUserProfileDocument(userAuth);
+            userRef.onSnapshot(snapShot => {
+                this.setState({
+                  currentUser:{
+                    id: snapShot.id,
+                    ...snapShot.data()
+                  }
+                });
+                console.log(this.state.currentUser);
+            });
+            
+         }
+         this.setState({currentUser: userAuth})
       })
   }
 
