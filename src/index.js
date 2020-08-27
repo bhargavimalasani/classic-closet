bdfc2d972a7e5c696118d9fe674cd96b51bfa0ce
Lohith Reddy Nallappagari <lohith@macbook-pro.home>
2020-08-27 10:13:01 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import App from './App';
+
+import store from './redux/store';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+  <Provider store={store}>
+  <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
+    
    document.getElementById('root'));
 
