c6b1fbaa4a634d2e9724e81a9b26f2b9125b7ca5
Lohith Reddy Nallappagari <lohith@macbook-pro.home>
2020-10-02 16:39:41 -0400

    added stripe payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HXuT0KHOMKauQb1vCXgyPetCVYKjV6VF7S05VLA7er77Cp73n0FsNESKVexKBZ8P6nsgEeF8KxHyi74VEP79N0f00UTrL70xS';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
